645bd04ac2ab79c2671f985e0b4320579114458c
RZ Code Studio <73451384+To-Code-Or-Not-To-Code@users.noreply.github.com>
2023-07-06 17:20:19 -0700

    Change - as space to +

--- thankyou.js
+++ thankyou.js
@@ -5,7 +5,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            app: url.searchParams.get("app").replace("-", " ")
+            app: url.searchParams.get("app").replace("+", " ")
         }
     }
 }).mount("html");
