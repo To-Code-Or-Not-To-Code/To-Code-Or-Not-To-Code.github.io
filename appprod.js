fbcb779c20981aa033e9db2b82d1b7bd7f680c8d
RZ Code Studio <73451384+To-Code-Or-Not-To-Code@users.noreply.github.com>
2023-07-23 19:58:01 -0700

    Animations

--- appprod.js
+++ appprod.js
@@ -14,3 +14,17 @@ const anim = KUTE.fromTo(
 );
 
 anim.start();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => observer.observe(el));
