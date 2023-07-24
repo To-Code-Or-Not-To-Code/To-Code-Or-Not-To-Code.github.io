2984f4cbfe9456f61883be0d9fff9a2a5639f2b6
RZ Code Studio <73451384+To-Code-Or-Not-To-Code@users.noreply.github.com>
2023-07-23 20:20:37 -0700

    Remove development code

--- appprod.js
+++ appprod.js
@@ -17,7 +17,6 @@ anim.start();
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
         } else {
